81ca4b98423c140f9b60a9d537d97fd1df3c2b34
Anthony Fink <40363736+psytonik@users.noreply.github.com>
2024-08-26 12:22:50 +0300

    fake user agent (chrome browser mac)

--- src/routes/route.ts
+++ src/routes/route.ts
@@ -18,7 +18,7 @@ router.post('/', async (req:Request,res: Response) => {
 		try {
 			const response = await axios.get(normalizedUrl,{
 				headers: {
-					'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
+					'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36'
 				}});
 			const html = await response.data;
 			let metadata: Metadata = extractMetadata(html);
